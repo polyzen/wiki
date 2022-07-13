87732dfb26864f91d55777ed6c2098d9179c2e46
Daniel M. Capella <polyzen@archlinux.org>
2022-07-13 18:01:20 -0400

    Use sphinxext-opengraph

--- conf.py
+++ conf.py
@@ -28,7 +28,10 @@ author = 'See contribution graph \
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = []
+extensions = ['sphinxext.opengraph']
+
+ogp_site_url = 'https://wiki.lacto.se'
+ogp_type = 'article'
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
