28992d51cdf1135fa7a14409016c44815969276d
Daniel M. Capella <polyzen@archlinux.org>
2025-07-20 18:58:48 -0400

    Update config
    Includes a fix for https://docs.astral.sh/ruff/rules/unused-import/.

--- conf.py
+++ conf.py
@@ -1,96 +1,55 @@
 # Configuration file for the Sphinx documentation builder.
 #
-# This file only contains a selection of the most common options. For a full
-# list see the documentation:
+# For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
-# -- Path setup --------------------------------------------------------------
-
-# If extensions (or modules to document with autodoc) are in another directory,
-# add these directories to sys.path here. If the directory is relative to the
-# documentation root, use os.path.abspath to make it absolute, like shown here.
-#
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
-
+from importlib.util import find_spec
 
 # -- Project information -----------------------------------------------------
 
 project = 'Wiki'
 
-
 # -- General configuration ---------------------------------------------------
 
-# Add any Sphinx extension module names here, as strings. They can be
-# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
-# ones.
-extensions = ['myst_parser', 'sphinxext.opengraph']
+extensions = [
+  'myst_parser',
+  'sphinxext.opengraph',
+]
 
-try:
-    import sphinxext.opengraph
-except ImportError:
+# Extension only used for deployments
+if find_spec('sphinxext') is None:
     extensions.remove('sphinxext.opengraph')
 
+exclude_patterns = ['_build']
+source_suffix = { '.md': 'markdown' }
+templates_path = ['_templates']
+
+# -- Options for MyST --------------------------------------------------------
+
 myst_enable_extensions = [
-    "colon_fence",
-    "deflist",
+    'colon_fence',
+    'deflist',
 ]
+
+# -- Options for Open Graph --------------------------------------------------
 
 ogp_site_url = 'https://wiki.lacto.se'
 ogp_type = 'article'
 
-# Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
-
-# The suffix(es) of source filenames.
-# You can specify multiple suffix as a list of string:
-#
-# source_suffix = ['.rst', '.md']
-source_suffix = { '.md': 'markdown' }
-
-# List of patterns, relative to source directory, that match files and
-# directories to ignore when looking for source files.
-# This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
-
-
 # -- Options for HTML output -------------------------------------------------
 
-# The theme to use for HTML and HTML Help pages.  See the documentation for
-# a list of builtin themes.
-#
 html_theme = 'furo'
-
 html_theme_options = {
-    "source_repository": "https://gitlab.com/polyzen/wiki",
-    "source_branch": "master",
-    "source_directory": "/",
+    'source_repository': 'https://gitlab.com/polyzen/wiki',
+    'source_branch': 'master',
+    'source_directory': '/',
 }
-
-
-# The name for this set of Sphinx documents.
-# "<project> v<release> documentation" by default.
-#
 html_title = project
-
-# The name of an image file (relative to this directory) to use as a favicon of
-# the docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
-# pixels large.
-#
 html_favicon = '_static/favicon.svg'
-
-# Add any paths that contain custom static files (such as style sheets) here,
-# relative to this directory. They are copied after the builtin static files,
-# so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
-
-# If false, no index is generated.
-#
 html_use_index = False
-
 html_copy_source = False
 
-# -- Options for the linkcheck builder ---------------------------------------
+# -- Options for link checker ------------------------------------------------
 
 linkcheck_ignore = ['https://tt-rss.org']
